31b54ce86fe59177164c7efabd214603e4100467
daihb <daihb@DESKTOP-R31MQAB>
2019-07-01 13:42:29 +0800

    第二篇：Spring Boot配置文件详解（50%）

---
+++ SpringBootDemo/springboot-config/src/main/java/com/dhb/springbootconfig/web/LucyController.java
@@ -0,0 +1,20 @@
+package com.dhb.springbootconfig.web;
+
+import com.dhb.springbootconfig.bean.ConfigBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableConfigurationProperties({ConfigBean.class})
+public class LucyController {
+
+    @Autowired
+    ConfigBean configBean;
+
+    @RequestMapping(value = "/lucy")
+    public String miya(){
+        return configBean.getGreeting()+" >>>>"+configBean.getName()+" >>>>"+ configBean.getUuid()+" >>>>"+configBean.getMax();
+    }
+}
