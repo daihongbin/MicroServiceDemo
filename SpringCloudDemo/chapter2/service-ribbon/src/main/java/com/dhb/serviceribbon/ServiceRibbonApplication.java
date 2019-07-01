7757aa5e9b38082b2f178d7955c86fc800058e25
daihb <daihb@DESKTOP-R31MQAB>
2019-07-01 11:17:01 +0800

    完成第二篇: 服务消费者（rest+ribbon）(Finchley版本)

---
+++ SpringCloudDemo/chapter2/service-ribbon/src/main/java/com/dhb/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,25 @@
+package com.dhb.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient //向服务中心注册
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced //表明这个restTemplate开启负载均衡
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
