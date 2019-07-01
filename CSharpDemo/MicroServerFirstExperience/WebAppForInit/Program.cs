a743a1eaad0e7fe04ed713c22266ec358548ac9e
daihb <daihb@DESKTOP-R31MQAB>
2019-07-01 14:30:52 +0800

    从控制台参数接受ip和应用程序端口的web程序的编写。

---
+++ CSharpDemo/MicroServerFirstExperience/WebAppForInit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppForInit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            //从控制台启动行读取ip和端口启动参数
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+            var ip = config["ip"];
+            var port = config["port"];
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls($"http://{ip}:{port}");
+        }
+            
+    }
+}
