6cf2da6ee58eb63b223a1d3446accca9f8918253
daihb <daihb@DESKTOP-R31MQAB>
2019-07-15 16:33:27 +0800

    想搞springboot的，但是由于公司网络不行，还是回家搞吧。

---
+++ SpringBootDemo/springboot-jdbc/src/main/java/com/dhb/springbootjdbc/SpringbootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.dhb.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJdbcApplication.class, args);
+    }
+
+}
