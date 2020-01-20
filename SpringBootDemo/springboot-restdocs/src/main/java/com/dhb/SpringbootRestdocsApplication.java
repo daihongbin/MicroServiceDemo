ca44d0e3e240baf1f56d737467ca3c1ba07a0a97
dhb <dhb@laptop.com>
2020-01-20 14:46:46 +0800

    这个RestDoc没测试成功

---
+++ SpringBootDemo/springboot-restdocs/src/main/java/com/dhb/SpringbootRestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.dhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestdocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestdocsApplication.class, args);
+    }
+
+}
