03a1a28ff4468b74ad0ee3b24c867ea6c4135561
dhb <dhb@laptop.com>
2020-01-22 13:07:50 +0800

    springboot整合rabbitmq

---
+++ SpringBootDemo/springboot-rabbitmq/src/main/java/com/dhb/Runner.java
@@ -0,0 +1,33 @@
+package com.dhb;
+
+import com.dhb.message.Receiver;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private final Receiver receiver;
+
+    private ConfigurableApplicationContext context;
+
+    public Runner(RabbitTemplate rabbitTemplate, Receiver receiver, ConfigurableApplicationContext context) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.receiver = receiver;
+        this.context = context;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(SpringbootRabbitmqApplication.queueName,"Hello from RabbitMQ!Happy new year");
+        receiver.getLatch().await(1000, TimeUnit.MILLISECONDS);
+        context.close();
+    }
+}
