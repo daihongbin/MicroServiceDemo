81d5acc05d9b0d7cc193beba56e36aa1338ffa10
dhb <dhb@laptop.com>
2020-01-22 17:15:32 +0800

    增加表单校验

---
+++ SpringBootDemo/springboot-validate-form/src/main/java/com/dhb/web/WebController.java
@@ -0,0 +1,33 @@
+package com.dhb.web;
+
+import com.dhb.entity.PersonForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.validation.Valid;
+
+@Controller
+public class WebController extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm){
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
