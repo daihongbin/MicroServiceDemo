3eea6c56e3d57a2a36a48d78134c9ba5afbc9e95
dhb <dhb@laptop.com>
2020-01-23 10:15:31 +0800

    提交表单

---
+++ SpringBootDemo/springboot-handling-form-submission/src/main/java/com/dhb/SpringbootHandlingFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.dhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHandlingFormSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHandlingFormSubmissionApplication.class, args);
+    }
+
+}
