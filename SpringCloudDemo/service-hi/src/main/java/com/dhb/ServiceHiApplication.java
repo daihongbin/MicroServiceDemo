53784ab7282fcd486410ca7e9e60390688209bcd
dhb <dhb@laptop.com>
2020-03-17 14:05:46 +0800

    Sleuth 服务追踪组件

---
+++ SpringCloudDemo/service-hi/src/main/java/com/dhb/ServiceHiApplication.java
@@ -0,0 +1,13 @@
+package com.dhb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+}
---
+++ SpringCloudDemo/service-hi/src/test/java/com/dhb/ServiceHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
